2ef83bb8e7d713e840865bba0067a0685d24bea6
AmmAr Shabaan <shabaanammar597@gmail.com>
2025-05-21 13:35:43 +0300

    Update checkout.jsx

--- src/checkout.jsx
+++ src/checkout.jsx
@@ -120,7 +120,6 @@ function Checkout({ cart, setcart, dop }) {
                       <span>
                         Quantity:
                         <input
-                          readOnly
                           type="number"
                           disabled
                           className="quantity-label"
